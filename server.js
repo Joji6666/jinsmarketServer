094709a11e85d5043de6e9d72fa81779c5e1dd12
mac <mac@macui-MacBook-Air.local>
2022-12-08 13:54:14 +0900

    서버 연습 끝

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 app.get("/products", (req, res) => {
   models.Product.findAll({
     order: [["createdAt", "DESC"]], // 작성시간 기준으로 정렬
@@ -25,17 +37,12 @@ app.get("/products", (req, res) => {
 
 app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, price, description, seller } = body;
+  const { name, price, description, seller, imageUrl } = body;
 
-  if (!name || !description || !price || !seller) {
-    res.send("모든 항목을 작성해주세요.");
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 항목을 작성해주세요.");
   }
-  models.Product.create({
-    name,
-    description,
-    price,
-    seller,
-  })
+  models.Product.create({ name, description, price, seller, imageUrl })
     .then((result) => {
       console.log("상품 생성 결과 :", result);
       res.send({
@@ -44,7 +51,7 @@ app.post("/products", (req, res) => {
     })
     .catch((err) => {
       console.error("error");
-      res.send("상품 업로드 에러 발생");
+      res.status(400).send("상품 업로드 에러 발생");
     });
 });
 
@@ -66,8 +73,16 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("에러발생");
+      res.status(400).send("에러발생");
     });
+});
+
+app.post("/images", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 app.listen(port, () => {
